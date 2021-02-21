c0c5e1931340b7cd7b01c4a541177a500c08780f
jdhxyy <jdhxyy@gmail.com>
2021-02-21 19:49:06 +0800

    新建

---
+++ dcompy/__init__.py
@@ -0,0 +1,7 @@
+from dcompy.main import load
+from dcompy.common import LoadParam
+from dcompy.callback import register
+from dcompy.rx import receive
+from dcompy.waitlist import call, call_async
+
+__version__ = '1.0'
